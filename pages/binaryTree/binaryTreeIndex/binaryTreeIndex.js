48c47ec18ec5bd9a18c09886eeecb0fc8bdf5af4
unknown <unknown@example.com>
2019-06-23 21:54:35 +0800

    v1.1.1
    添加了二叉树与算法问题的下拉菜单

--- pages/binaryTree/binaryTreeIndex/binaryTreeIndex.js
+++ pages/binaryTree/binaryTreeIndex/binaryTreeIndex.js
@@ -1,66 +1,38 @@
-// pages/binaryTree/binaryTreeIndex/binaryTreeIndex.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    list: [
+      {
+        id: 'Insert',
+        name: '插入',
+        open: false,
+        pages: ['Min', 'Max', 'Middle']
+      }, {
+        id: 'Delete',
+        name: '删除',
+        open: false,
+        pages: ['None Child', 'One Child', 'Two Child']
+      }
+    ]
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  kindToggle(e) {
+    const id = e.currentTarget.id
+    const list = this.data.list
+    for (let i = 0, len = list.length; i < len; ++i) {
+      if (list[i].id === id) {
+        list[i].open = !list[i].open
+      } else {
+        list[i].open = false
+      }
+    }
+    this.setData({
+      list
+    })
+    wx.reportAnalytics('click_view_programmatically', {})
+  },
   onLoad: function (options) {
 
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
   }
+
 })
--- pages/problems/problemsIndex/problemsIndex.js
+++ pages/problems/problemsIndex/problemsIndex.js
@@ -1,66 +1,38 @@
-// pages/problems/problemsIndex/problemsIndex.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    list: [
+      {
+        id: 'Insert',
+        name: '回溯',
+        open: false,
+        pages: ['Queen', 'Maze']
+      }, {
+        id: 'Delete',
+        name: '分治',
+        open: false,
+        pages: ['Hanoi', '二分搜索']
+      }
+    ]
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  kindToggle(e) {
+    const id = e.currentTarget.id
+    const list = this.data.list
+    for (let i = 0, len = list.length; i < len; ++i) {
+      if (list[i].id === id) {
+        list[i].open = !list[i].open
+      } else {
+        list[i].open = false
+      }
+    }
+    this.setData({
+      list
+    })
+    wx.reportAnalytics('click_view_programmatically', {})
+  },
   onLoad: function (options) {
 
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
   }
+
 })
